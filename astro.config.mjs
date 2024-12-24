a6c5e5200e21c7429dd3cc95705bcd5c1192a442
Juan <Jinbyos@Gmail.com>
2024-12-24 00:30:56 -0300

    Changed styles and add astro auth

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import auth from 'auth-astro';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), auth()],
   devToolbar: {
     enabled: false
   }
