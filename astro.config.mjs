c978133ecc943876fc2dcac91435731e7e1aaa52
Juan <Jinbyos@Gmail.com>
2025-01-05 23:13:29 -0300

    Added netlify adapter and removed astro db

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,17 @@ import tailwind from '@astrojs/tailwind';
 
 import auth from 'auth-astro';
 
+import netlify from '@astrojs/netlify';
+
+import db from '@astrojs/db';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), auth()],
+  integrations: [tailwind(), auth(), db()],
+  output: 'server',
   devToolbar: {
     enabled: false
-  }
-  
+  },
+
+  adapter: netlify()
 });
