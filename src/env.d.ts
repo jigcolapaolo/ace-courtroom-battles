ae33f0c145eeb18651e26a5d7238aed98f74c267
Juan <Jinbyos@Gmail.com>
2025-01-16 19:25:15 -0300

    Added new types

--- src/env.d.ts
+++ src/env.d.ts
@@ -27,3 +27,48 @@ export interface CharacterRanking {
     totalRankings: number;
     ranks: Record<string, number>;
 }
+
+export interface Case {
+    id: number,
+    caseName: string,
+    incidentType: string,
+    victim: string,
+    cause: string
+}
+
+export interface Courtroom {
+    id: number;
+    name: string;
+    imgUrl: string;
+    theme: string;
+}
+
+type OpeningStatementLine = {
+    speaker: string;
+    line: string;
+};
+
+type SceneLink = {
+    id: number;
+    iframe: string;
+    description: string;
+}
+
+type Sprites = {
+    idle: string;
+    talking: string;
+}
+
+export interface Battle {
+    id: string,
+    case: Case,
+    attorney: string,
+    attorneySpriteUrls: Sprites,
+    prosecutor: string,
+    prosecutorSpriteUrls: Sprites,
+    courtroom: Courtroom,
+    room: string,
+    game: string,
+    scenesLinks: SceneLink[],
+    openingStatementLines: OpeningStatementLine[]
+}
