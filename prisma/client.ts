e649801f7263248997aadd7d99d687ea2a3a0907
Juan <Jinbyos@Gmail.com>
2025-01-18 16:39:17 -0300

    Changed prisma to use turso on prod. and sql server on dev.

--- prisma/client.ts
+++ prisma/client.ts
@@ -1,11 +1,35 @@
 import { PrismaClient } from '@prisma/client';
 import { PrismaLibSQL } from '@prisma/adapter-libsql'
 import { createClient } from '@libsql/client'
+import dotenv from 'dotenv';
 
-const libsql = createClient({
-  url: `${process.env.TURSO_DB_REMOTE_URL}`,
-  authToken: `${process.env.TURSO_DB_APP_TOKEN}`,
-})
+// const libsql = createClient({
+//   url: `${process.env.TURSO_DB_REMOTE_URL}`,
+//   authToken: `${process.env.TURSO_DB_APP_TOKEN}`,
+// })
 
-const adapter = new PrismaLibSQL(libsql)
-export const prisma = new PrismaClient({ adapter })
+// const adapter = new PrismaLibSQL(libsql)
+
+// export const prisma = new PrismaClient({ adapter })
+
+if (process.env.NODE_ENV === 'production') {
+  dotenv.config({ path: '.env.production' });
+} else {
+  dotenv.config();
+}
+
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === 'production') {
+  const libsql = createClient({
+    url: `${process.env.DATABASE_URL}`,
+    authToken: `${process.env.TURSO_DB_APP_TOKEN}`,
+  });
+
+  const adapter = new PrismaLibSQL(libsql);
+  prisma = new PrismaClient({ adapter });
+} else {
+  prisma = new PrismaClient();
+}
+
+export { prisma };
