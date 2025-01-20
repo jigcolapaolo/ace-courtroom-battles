95e9c9728503059bc4858f8c4273a60ff368fd1f
Juan <Jinbyos@Gmail.com>
2025-01-20 17:14:29 -0300

    Changed env variables for db connection

--- prisma/client.ts
+++ prisma/client.ts
@@ -2,8 +2,9 @@ import { PrismaClient } from '@prisma/client';
 import { PrismaLibSQL } from '@prisma/adapter-libsql'
 import { createClient } from '@libsql/client'
 
+//--Using Turso DB--
 const libsql = createClient({
-  url: `${process.env.DATABASE_URL}`,
+  url: `${process.env.TURSO_DATABASE_URL}`,
   authToken: `${process.env.TURSO_DB_APP_TOKEN}`,
 })
 
@@ -12,8 +13,6 @@ const adapter = new PrismaLibSQL(libsql)
 export const prisma = new PrismaClient({ adapter })
 
 
-
-
-// prisma = new PrismaClient();
+//---Using Local DB Sqlserver---
 
 // export const prisma = new PrismaClient();
